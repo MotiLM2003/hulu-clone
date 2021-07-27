b47b7b313b1b7191e58d7f73ea8c57d6d24190a2
Moti Elmakies <motiphone2003@gmail.com>
2021-07-27 20:49:05 +0300

    :apple progress

--- components/Results.js
+++ components/Results.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Thumbnail from './Thumbnail';
 
-const Results = () => {
-  return <div>results</div>;
+const Results = ({ results }) => {
+  const BASE_URL = 'https://image.tmdb.org/t/p/original/';
+  return (
+    <div>
+      {results.map((result) => {
+        return <Thumbnail key={result.id} result={result} />;
+      })}
+    </div>
+  );
 };
 
 export default Results;
