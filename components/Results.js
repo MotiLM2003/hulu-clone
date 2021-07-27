da1c8823a33e239987b3d32c47044bd252b5625d
Moti Elmakies <motiphone2003@gmail.com>
2021-07-27 21:21:23 +0300

    final

--- components/Results.js
+++ components/Results.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Thumbnail from './Thumbnail';
-
+import FlipMove from 'react-flip-move';
 const Results = ({ results }) => {
-  const BASE_URL = 'https://image.tmdb.org/t/p/original/';
   return (
-    <div>
+    <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
       {results.map((result) => {
         return <Thumbnail key={result.id} result={result} />;
       })}
-    </div>
+    </FlipMove>
   );
 };
 
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,14 @@
 import { ThumbUpIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 import React from 'react';
-
-const Thumbnail = ({ result }) => {
+import { forwardRef } from 'react';
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/';
   return (
-    <div className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover-z-50'>
+    <div
+      ref={ref}
+      className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover-z-50'
+    >
       <Image
         layout='responsive'
         width={1920}
@@ -29,6 +32,6 @@ const Thumbnail = ({ result }) => {
       </div>
     </div>
   );
-};
+});
 
 export default Thumbnail;
